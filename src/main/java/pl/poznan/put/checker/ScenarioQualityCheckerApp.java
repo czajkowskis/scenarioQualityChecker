c74ead13220554fc28485953888713bce4c75c60
Laura Sałata <salatalaura1@gmail.com>
2024-05-06 11:14:36 +0200

    Hello World!

---
+++ src/main/java/pl/poznan/put/checker/ScenarioQualityCheckerApp.java
@@ -0,0 +1,11 @@
+package pl.poznan.put.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenarioQualityCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
