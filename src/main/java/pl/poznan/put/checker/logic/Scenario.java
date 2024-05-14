95b1c0fc04a704d9801949574451dee74dcc27b7
szymon <szymon@localhost.localdomain>
2024-05-14 23:15:42 +0200

    Changed represantion of Scenarios and Steps in code

---
+++ src/main/java/pl/poznan/put/checker/logic/Scenario.java
@@ -0,0 +1,36 @@
+package pl.poznan.put.checker.logic;
+
+import pl.poznan.put.checker.logic.visitors.Visitable;
+import pl.poznan.put.checker.logic.visitors.Visitor;
+
+import java.util.Arrays;
+import java.util.List;
+
+public record Scenario(String title, List<String> externalActors, List<String> internalActors, List<Step> steps) implements Visitable {
+    public static List<String> KEYWORDS = Arrays.asList("ELSE:", "FOR EACH:", "IF:");
+
+    public Scenario {
+        if(title == null) {
+            throw new IllegalArgumentException("Title cannot be null");
+        }
+        if(title.isEmpty()) {
+            throw new IllegalArgumentException("Title cannot be empty");
+        }
+        if(externalActors == null) {
+            throw new IllegalArgumentException("ExternalActors cannot be null");
+        }
+        if(internalActors == null) {
+            throw new IllegalArgumentException("InternalActors cannot be null");
+        }
+        if(steps == null) {
+            throw new IllegalArgumentException("Steps cannot be null");
+        }
+        if(steps.isEmpty()) {
+            throw new IllegalArgumentException("Steps cannot be empty");
+        }
+    }
+
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+}
--- src/main/java/pl/poznan/put/checker/logic/Step.java
+++ src/main/java/pl/poznan/put/checker/logic/Step.java
@@ -1,12 +1,21 @@
 package pl.poznan.put.checker.logic;
 
-public record Step(String text, SubScenario subScenario) {
-    public Step {
+import pl.poznan.put.checker.logic.visitors.Visitable;
+import pl.poznan.put.checker.logic.visitors.Visitor;
+
+import java.util.List;
+
+public record Step(String text, List<Step> subSteps) implements Visitable {
+    public Step  {
         if (text == null) {
             throw new IllegalArgumentException("Step text cannot be null");
         }
         if(text.isEmpty()) {
             throw new IllegalArgumentException("Step text cannot be empty");
         }
+    }
+
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
     }
 }
