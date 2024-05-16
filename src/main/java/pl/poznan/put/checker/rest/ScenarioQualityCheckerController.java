47db1aa43e91a2277c7f61542fcad63174b5a983
Laura Sałata <salatalaura1@gmail.com>
2024-05-16 17:28:51 +0200

    Correct MainScenario to Scenario

--- src/main/java/pl/poznan/put/checker/rest/ScenarioQualityCheckerController.java
+++ src/main/java/pl/poznan/put/checker/rest/ScenarioQualityCheckerController.java
@@ -3,7 +3,7 @@ package pl.poznan.put.checker.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-import pl.poznan.put.checker.logic.MainScenario;
+import pl.poznan.put.checker.logic.Scenario;
 
 @RestController
 @RequestMapping("/")
@@ -12,7 +12,7 @@ public class ScenarioQualityCheckerController {
     private static final Logger logger = LoggerFactory.getLogger(ScenarioQualityCheckerController.class);
 
     @PostMapping(path = "echo", produces = "application/json")
-    public MainScenario echo(@RequestBody MainScenario body) {
+    public Scenario echo(@RequestBody Scenario body) {
         logger.info("Received echo request: {}", body);
 
         return body;
