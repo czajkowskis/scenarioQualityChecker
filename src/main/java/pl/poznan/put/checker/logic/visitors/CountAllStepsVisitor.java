06d074a504e4794d908f0456530a0cfa7d51c6f0
magdaa08 <madzia0804@gmail.com>
2024-05-17 23:19:57 +0200

    Updated Visitor counting all steps with Javadoc

--- src/main/java/pl/poznan/put/checker/logic/visitors/CountAllStepsVisitor.java
+++ src/main/java/pl/poznan/put/checker/logic/visitors/CountAllStepsVisitor.java
@@ -1,17 +1,34 @@
 package pl.poznan.put.checker.logic.visitors;
 
-
 import pl.poznan.put.checker.logic.Step;
 
 
-
+/**
+ * A visitor that counts all steps in a process.
+ * This class extends AbstractCountStepsVisitor and overrides the necessary
+ * methods to provide the functionality of counting every step encountered.
+ */
 public class CountAllStepsVisitor extends AbstractCountStepsVisitor{
 
+
+    /**
+     * Creates a new instance of CountAllStepsVisitor.
+     * This method is used by the superclass to create a new visitor instance when required.
+     *
+     * @return A new instance of CountAllStepsVisitor.
+     */
     @Override
     protected CountAllStepsVisitor createVisitor() {
         return new CountAllStepsVisitor();
     }
 
+    /**
+     * Determines whether the given step should be counted.
+     * In this implementation, every step is counted.
+     *
+     * @param step The step to check.
+     * @return true, indicating that the step should always be counted.
+     */
     @Override
     protected boolean shouldCountStep(Step step) {
         return true;
