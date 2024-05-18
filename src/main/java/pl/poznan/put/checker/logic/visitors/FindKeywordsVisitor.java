6a6bd53fc377f2d910f31d35e7fab005f7c528f1
mmartyna123 <mmartyna.stasiak@gmail.com>
2024-05-18 12:29:33 +0200

    Updated finding keywords visitor class with Javadoc

--- src/main/java/pl/poznan/put/checker/logic/visitors/FindKeywordsVisitor.java
+++ src/main/java/pl/poznan/put/checker/logic/visitors/FindKeywordsVisitor.java
@@ -3,13 +3,33 @@ package pl.poznan.put.checker.logic.visitors;
 import pl.poznan.put.checker.logic.Scenario;
 import pl.poznan.put.checker.logic.Step;
 
+
+/**
+ * A visitor implementation that checks whether any step in a scenario
+ * begins with a specified keyword. Implements the Visitor interface.
+ */
 public class FindKeywordsVisitor implements Visitor {
     private boolean beginsWithKeyword = false;
+
+
+    /**
+     * Visits a Scenario. 
+     *
+     * @param scenario the scenario to visit.
+     */
     @Override
     public void visit(Scenario scenario) {
 
     }
 
+
+    /**
+     * Visits a Step and checks if the step's text begins with any of the
+     * specified keywords. If a keyword match is found, the beginsWithKeyword
+     * is set to true.
+     *
+     * @param step the step to visit and check for keywords.
+     */
     @Override
     public void visit(Step step) {
         for (String keyword : Scenario.KEYWORDS) {
@@ -20,6 +40,13 @@ public class FindKeywordsVisitor implements Visitor {
         }
 
     }
+
+
+    /**
+     * Returns if any step text begins with a specified keyword.
+     *
+     * @return true if a step text begins with a keyword, false otherwise.
+     */
     public boolean getBeginsWithKeyword() {
         return beginsWithKeyword;
     }
